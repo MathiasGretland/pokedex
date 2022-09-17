d30eaa919d76fcec261e9947aa0a1e443692f503
MathiasGretland <mathiasgretland@gmail.com>
2022-09-18 01:38:53 +0200

    Finished popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,6 +1,9 @@
 import { Modal } from '@material-ui/core'
 import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGaugeHigh, faHammer, faHeart, faShield, faShieldHalved, faWandMagicSparkles} from "@fortawesome/free-solid-svg-icons";
+
 
 const Popup = ( {open, setOpen, data} ) => {
     
@@ -35,17 +38,18 @@ const Popup = ( {open, setOpen, data} ) => {
     >
         <div className="popup">
             <h1>{data.species.name.charAt(0).toUpperCase() + data.species.name.slice(1)}</h1>
+            <Button onClick={newImageHandler} variant="outlined" >NEXT</Button>
             <div className="info">
                 <div className="abilities">
                     <div className="textBox">
-                        <h3>HP</h3>
-                        <h3>ATTACK</h3>
-                        <h3>DEFENSE</h3>
-                        <h3>SPECIAL ATTACK</h3>
-                        <h3>SPECIAL DEFENSE</h3>
-                        <h3>SPEED</h3> 
+                        <h3><FontAwesomeIcon icon={faHeart} />HP</h3>
+                        <h3><FontAwesomeIcon icon={faHammer} />ATTACK</h3>
+                        <h3><FontAwesomeIcon icon={faShield} /> DEFENSE</h3>
+                        <h3><FontAwesomeIcon icon={faWandMagicSparkles} />SPECIAL ATTACK</h3>
+                        <h3><FontAwesomeIcon icon={faShieldHalved} />SPECIAL DEFENSE</h3>
+                        <h3><FontAwesomeIcon icon={faGaugeHigh} />SPEED</h3> 
                     </div>
-                    <div className="textBox">
+                    <div className="textBox" >
                         <h3>{data.stats[0].base_stat}</h3>
                         <h3>{data.stats[1].base_stat}</h3>
                         <h3>{data.stats[2].base_stat}</h3>
@@ -56,7 +60,6 @@ const Popup = ( {open, setOpen, data} ) => {
                 </div>
                 <div className="picture-rotation" >
                     <img src={selectedImage} alt={data.species.name} />
-                    <Button onClick={newImageHandler} variant="contained" color="primary" >NEXT</Button>
                 </div>
             </div>
         </div>
